7271cb405e2e997a5e26ce44c76e8d5c24085ffa
Jason Kennaly <jkennaly@gmail.com>
2022-01-13 15:13:50 -0800

    store jobIds in Redis

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-console.log('Loading function');
 
 const aws = require('aws-sdk');
+const redis = require("redis")
+const client = redis.createClient({url: process.env.REDIS_URL})
 
 const textract = new aws.Textract();
 
@@ -11,6 +12,7 @@ exports.handler = async (event, context) => {
     // Get the object from the event and show its content type
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+
     var params = {
         DocumentLocation: {
           S3Object: {
@@ -21,12 +23,22 @@ exports.handler = async (event, context) => {
         FeatureTypes: ['FORMS'],
         OutputConfig: {
             S3Bucket: bucket,
-            S3Prefix: 'textractedLineups',
-        },
-        JobTag: key
+            S3Prefix: 'textractedLineups'
+        }
     }
-    console.log('startDocumentAnalysis params', params)
+    //console.log('startDocumentAnalysis params', params)
     const job = await textract.startDocumentAnalysis(params).promise()
     console.log('startDocumentAnalysis result', job)
+    if(!job || !job.JobId) {
+    	console.error('No job idResult')
+    	console.error(params)
+    	return job	
+    } 
+    const leKey = `textraction.${job.JobId}`
+    const upd = await client.connect()
+    const res = await client.set(leKey, key, {
+			EX: 60 * 60 * 24 * 30
+		})
+
     return job
 };
