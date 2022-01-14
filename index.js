c71f56df970ea9f6aa5dcedcb2a3f3793bcefd88
Jason Kennaly <jkennaly@gmail.com>
2022-01-13 16:24:06 -0800

    store lineups in Redis

--- index.js
+++ index.js
@@ -12,33 +12,29 @@ exports.handler = async (event, context) => {
     // Get the object from the event and show its content type
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-
-    var params = {
-        DocumentLocation: {
-          S3Object: {
+    let result;
+    try {
+        const params = {
             Bucket: bucket,
-            Name: key,
-          },
-        },
-        FeatureTypes: ['FORMS'],
-        OutputConfig: {
-            S3Bucket: bucket,
-            S3Prefix: 'textractedLineups'
-        }
-    }
-    //console.log('startDocumentAnalysis params', params)
-    const job = await textract.startDocumentAnalysis(params).promise()
-    console.log('startDocumentAnalysis result', job)
-    if(!job || !job.JobId) {
-    	console.error('No job idResult')
-    	console.error(params)
-    	return job	
-    } 
-    const leKey = `textraction.${job.JobId}`
-    const upd = await client.connect()
-    const res = await client.set(leKey, key, {
-			EX: 60 * 60 * 24 * 30
+            Key: key
+        };
+        
+        result = await s3.getObject(params).promise();
+        if(!result || !result.Body || ! result.Body.toString('utf-8')) throw new Error('invalid S3 object')
+	    const full = JSON.parse(result)
+		const lineup = full.Blocks
+			.filter(b => b.BlockType === 'LINE')
+			.map(b => b.Text)
+	    const leKey = 'arach-lineup.' + key
+	    await client.connect()
+	  	await client.set(leKey, lineup, {
+			EX: 3600 * 24 * 30
 		})
+	    client.quit()
+    } catch (error) {
+        console.log(error);
+        return;
+    }
 
     return job
 };
